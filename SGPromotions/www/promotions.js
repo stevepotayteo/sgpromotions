0c709790dfd2a631fb626d7403dd616f6d365e4f
Steve Teo <xylancer@gmail.com>
2012-02-19 19:07:05 +0800

    Converted current dynamic html generation to JSRender.
    Signed-off-by: Steve Teo <xylancer@gmail.com>

--- SGPromotions/www/promotions.js
+++ SGPromotions/www/promotions.js
@@ -15,22 +15,9 @@ function getNearby() {
 
 function displayNearby(data) {
 	if(data.query.results.json != null){
-		var nearby = data.query.results.json.results;
-		var nearbyHtml = '<ul data-role="listview" id="nearby-list" data-theme="c" data-inset="true" data-filter="true">';
-		for(n in nearby){
-			var item = nearby[n];
-			nearbyHtml += '<li>';
-			nearbyHtml += '<a href="'+ item.url +'">';
-			nearbyHtml += '<img src="'+ item.pic +'" width="24" height="24" class="ui-li-icon"/>'; // have to manual specify the width/height
-			nearbyHtml += '<h3>' + item.name + '</h3>';
-			nearbyHtml += '<p>' + item.promo + '</p>';
-			nearbyHtml += '<span class="ui-li-count">' + item.distance + 'm</span>';
-			nearbyHtml += '</a>';
-			nearbyHtml += '</li>';
-		}
-		nearbyHtml += '</ul>';
-		$('#nearby-content').html(nearbyHtml) // trigger("pagecreate").trigger("refresh"); // alternative method for multiple generated items
-		$('#nearby-content > ul').listview(); // needed for ajax addition
+		var nearby = data.query.results.json;
+		$("#nearby-content").html($("#NearbyTemplate").render(nearby));
+		$('#nearby-content > ul').listview();
 	}
 }
 
@@ -47,22 +34,8 @@ function getCategory() {
 
 function displayCategory(data) {
 	if(data.query.results.json != null){
-		var category = data.query.results.json.results;
-		var categoryHtml = '<ul data-role="listview" id="category-list" data-theme="c" data-inset="true" data-filter="true">';
-		for(c in category){
-			var item = category[c];
-			categoryHtml += '<li>';
-			categoryHtml += '<a href="'+ item.categorytag +'">';
-			categoryHtml += '<img src="'+ item.iconurl +'" width="16" height="16" class="ui-li-icon"/>'; // have to manual specify the width/height
-			categoryHtml += item.category;
-			categoryHtml += '</a>';
-			categoryHtml += '</li>';
-		}
-		categoryHtml += '</ul>';
-		$('#category-content').html(categoryHtml) // trigger("pagecreate").trigger("refresh"); // alternative method for multiple generated items
-		$('#category-content > ul').listview(); // needed for ajax addition
+		var category = data.query.results.json;
+		$("#category-content").html($("#CategoryTemplate").render(category)); // trigger("pagecreate").trigger("refresh"); // alternative method for multiple generated items
+		$('#category-content > ul').listview();
 	}
 }
-
-
-//categoryHtml += '<span class="ui-li-count">4</span>'; // good to keep count
