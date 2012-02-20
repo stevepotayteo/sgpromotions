dbb969d64924ff4eec03e108e4d44b857ba1f49a
KellTainer <xylancer@gmail.com>
2012-02-20 20:27:19 +0800

    Added PhoneGapMock to simulate phonegap in browsers, changed application icons to the applications and changed the description of the app in the manifest.

---
+++ SGPromotions/www/phonegapMock.js
@@ -0,0 +1,13 @@
+﻿document.addEventListener = function (evt, handler, capture) {
+  $("body").bind(evt, handler);
+};
+
+$(document).ready(function () {
+  setTimeout(function () {
+    $("body").trigger("deviceready");
+  }, 100);
+});
+
+
+
+
